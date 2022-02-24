c33906e898923f46315608be57ec8293f985f8f8
Natalia <natalia.s36.ns@gmail.com>
2022-02-24 20:30:22 -0300

    Model, Repository e Controller farmacia.

---
+++ src/main/java/com/generation/farmacia/Repository/ProdutoRepository.java
@@ -0,0 +1,23 @@
+package com.generation.farmacia.Repository;
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.farmacia.Model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository <Produto, Long> {
+	
+	public List <Produto> findAllByNomeContainingIgnoreCase (String nome);
+
+	public List <Produto> findAllByPrecoGreaterThanOrderByPreco (BigDecimal preco);
+	
+	public List <Produto> findAllByPrecoLessThanOrderByPreco (BigDecimal preco);
+	
+	public List <Produto> findAllByNomeContainingIgnoreCaseAndLaboratorioContainingIgnoreCase (String nome, String laboratorio);
+	
+	public List <Produto> findAllByPrecoBetweenOrderByPreco(BigDecimal preco1, BigDecimal preco2);
+}
+
